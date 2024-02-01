7720d4839e5d40fdbdd3b4194db9746a8fe2a756
ArthurKagwa <asasiraarthur@gmail.com>
2024-02-01 18:50:08 +0300

    Clean up

--- User/User.java
+++ User/User.java
@@ -1,6 +1,7 @@
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.text.ParseException;
 
 
 
@@ -29,29 +30,30 @@ User()
     System.out.println("Enter password");
     this.password = scanObj.nextLine();
     status=0;
-    // System.out.println("Enter your last name");
-    // this.lastName = sc.nextLine();
-    // System.out.println("Enter your email");
-    // this.email = sc.nextLine();
+    System.out.println("Enter your last name");
+    this.lastName = scanObj.nextLine();
+    System.out.println("Enter your email");
+    this.email = scanObj.nextLine();
     
-    // System.out.println("Enter your username");
-    // this.username = sc.nextLine();
-    // System.out.println("Enter your phone number");
-    // this.phoneNumber = sc.nextLine();
-    // System.out.println("Enter your address");
-    // this.address = sc.nextLine();
-    // System.out.println("Enter your date of birth (yyyy-MM-dd)");
-    // String dateOfBirthStr = sc.nextLine();
+    System.out.println("Enter your username");
+    this.username = scanObj.nextLine();
+    System.out.println("Enter your phone number");
+    this.phoneNumber = scanObj.nextLine();
+    System.out.println("Enter your address");
+    this.address = scanObj.nextLine();
+    System.out.println("Enter your date of birth (yyyy-MM-dd)");
+    String dateOfBirthStr = scanObj.nextLine();
 
-    // try {
-    //     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    //     this.dateOfBirth = dateFormat.parse(dateOfBirthStr);
-    // } catch (ParseException e) {
-    //     System.out.println("Invalid date format. Please enter the date in the format yyyy-MM-dd.");
-    //     System.out.println("Enter your date of birth (yyyy-MM-dd)");
-    //     dateOfBirthStr = sc.nextLine();
+    try {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        this.dateOfBirth = dateFormat.parse(dateOfBirthStr);
+    } catch (ParseException e) {
+        System.out.println("Invalid date format. Please enter the date in the format yyyy-MM-dd.");
+        System.out.println("Enter your date of birth (yyyy-MM-dd)");
+        dateOfBirthStr = scanObj.nextLine();
         
-        }
+    }
+}
 
 static  Admin registerAdmin()
     {
@@ -72,7 +74,7 @@ static  Admin registerAdmin()
             String fname=scanObj.nextLine();
             System.out.println(adminArrayList.get(0).firstName);
             for (Admin admin : adminArrayList) {
-                if(admin.status==0)
+                if(admin.firstName.equals(fname))
                 {
                     System.out.println("Enter password for" +admin.firstName);
                     String pass=scanObj.nextLine();
@@ -86,57 +88,32 @@ static  Admin registerAdmin()
                     {
                         System.out.println("FAILED");
                     }
-                    
-                    
                 }
-               
-                
+    
             }
-           
-    }
-    // static void login(ArrayList <Admin> adminArrayList)
-    // {
-    //     System.out.println("Enterb first name");
-    //     String fname=scanObj.nextLine();
+        }
+          
+    
 
-        
-    // }
 public static void main(String[] args) {
     System.out.println("Welcome to the loan management system");
     System.out.println("Do you want to login or register an Admin");
     System.out.println("1. Login");
     System.out.println("2. Register");
-    Scanner scs = new Scanner(System.in);
-    int choice = scs.nextInt();
+    
+    int choice = scanObj.nextInt();
    
    
 
     ArrayList<Admin> admins = new ArrayList<Admin>();
-    // if(choice==2)
-    // {
-    //     admins.add(registerAdmin());
-    //     System.out.println("Do you want to register another Admin");
-    //     System.out.println("2 Yes" + "1 No");
-    // }
-    // System.out.println("Do you want to login or register an Admin");
-    // System.out.println("1. Login");
-    // System.out.println("2. Register");
-    // choice = scs.nextInt();
-    // if(choice==1)
-    // {
-    // logAdmin(admins);
-    // }
-    // else
-    // {
-    //     admins.add(registerAdmin());
-    // }
+   
     
     while(choice==2)
     {
         admins.add(registerAdmin());
         System.out.println("Do you want to register another Admin");
         System.out.println("2 Yes" + "1 No");
-        choice = scs.nextInt();
+        choice = scanObj.nextInt();
     }
     if(choice==1)
     {
